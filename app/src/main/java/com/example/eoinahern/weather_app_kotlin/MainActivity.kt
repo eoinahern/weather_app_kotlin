777dbafc7af714ee0a3204b8c38a922170ad5df6
eoinahern <eoin.ahern@nycit.ie>
2016-12-21 15:50:26 +0000

    anko async call. bollox

--- app/src/main/java/com/example/eoinahern/weather_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/eoinahern/weather_app_kotlin/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.eoinahern.weather_app_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.doAsync
 
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.find
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.custom.async
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,9 +23,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastlist : RecyclerView = find(R.id.forecast_list)
-        forecastlist.layoutManager = LinearLayoutManager(this)
-        forecastlist.adapter = ForecastListAdapter(items)
+        //val forecastlist : RecyclerView = find(R.id.forecast_list)
+        //forecastlist.layoutManager = LinearLayoutManager(this)
+        //forecastlist.adapter = ForecastListAdapter(items)
+
+
+        val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+        val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
+        val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
+
+
+        async() {
+            Request(COMPLETE_URL + "94101").run()  //using hard coded zip code
+            uiThread { longToast("Request performed") }
+        }
+
     }
 
 }
